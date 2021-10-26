6a39af77f96434778c2d033b148ec1bdfff37b3a
Fillp <fillio00@wp.pl>
2021-10-26 11:07:51 +0200

    test with Encryption and Decryption

---
+++ encryption.py
@@ -0,0 +1,18 @@
+import base64
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+
+password_provided = "password"  # <- klucz do odszywrowania plików, jeżeli wprowadze złe hasło to plik nie odszyfruje się poprawnie
+password = password_provided.encode()  # <- szyfrowanie
+salt = b'\xe1\xfc#\x9f\x14o\xc9\xea\xce\xdf\x1f\xc60k\x03\xfe'
+kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=100000,
+    backend=default_backend()
+)
+key = base64.urlsafe_b64encode(kdf.derive(password))  # Can only use kdf once
+print(key)
--- main.py
+++ main.py
@@ -10,20 +10,22 @@ root.resizable(width=False, height=False)
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
-frame = tk.Frame(root, bg='#cbc8c8')
+frame = tk.Frame(root, bg=FRAME_BG_COLOUR)
 frame.place(relx=0.1, rely=0.3, relwidth=0.8, relheight=0.5)
 
 label = tk.Label(root, text='Odblokuj bazę danych', font=24)
 label.place(relx=0.1, rely=0.2)
 
 label3 = tk.Label(frame, text='Wprowadź hasło: ', bg=FRAME_BG_COLOUR)
-label3.grid(row=0, column=0)
+label3.place(relx=0.05, rely=0.1)
 
 entry = tk.Entry(frame)
-entry.grid(row=0, column=1)
+entry.place(relx=0.2, rely=0.1)
 
 label2 = tk.Label(root, text='© powered by Filip Walkowicz')
 label2.pack()
+
+passwd = input("password: ")
 
 if __name__ == "__main__":
     # run an APP
